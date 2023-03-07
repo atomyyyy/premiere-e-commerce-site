2e6e0fbac3f2f17c2a36e25136ad694a33909450
atomyyyy <atomyuen95@gmail.com>
2023-03-07 11:22:51 +0800

    feat: add functions

---
+++ netlify/functions/checkout/checkout.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+
+const { MongoClient } = require('mongodb');
+const crypto = require('crypto');
+
+const url = process.env.DATABASE_CONNECTION_STRING;
+const client = new MongoClient(url);
+
+const handler = async (event) => {
+  if (event.httpMethod !== 'POST') {
+    return {
+      body: JSON.stringify({ message: 'BAD_REQUEST' }),
+      headers: { 'Content-Type': 'application/json'},
+      statusCode: 400
+    }
+  }
+
+  await client.connect();
+  const db = client.db('e_commerce');
+  const collection = db.collection('order');
+
+  const orderId = crypto.randomUUID();
+  collection.insertOne({
+    ...JSON.parse(event.body),
+    orderId
+  })
+
+  return {
+    body: JSON.stringify(createdDocument),
+    headers: { 'Content-Type': 'application/json'},
+    statusCode: createdDocument ? 201 : 500
+  }
+}
+
+module.exports = { handler }
